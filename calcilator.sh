e0a723f4b4c6961898fe847f41cc11b7bb197772
vishal sam <vinishakr9@gmail.com>
2022-01-04 19:08:45 +0530

    added function featured and reframed

--- calcilator.sh
+++ calcilator.sh
@@ -1,12 +1,45 @@
+functon sum() {
+   sum=$(($a+$b))
+   echo "sum is $sum"
+}
+   functon sub() {
+   sub=$(($a-$b))
+   echo "diffrence is $sub"
+}
+functon mul() {
+   mul=$(($a*$b))
+   echo "multiplication  is $mul"
+}
+functon div() {
+   div=$(($a/$b))
+   echo "division is $div"
+}
+
+
+
+
+
+
 echo "enter a number"
 read a
 echo "enter 2nd number"
 read b
-sum=$(($a+$b))
-echo "sum is $sum"
-sub=$(($a-$b))
-echo "diffrence is $sub"
-mul=$(($a*$b))
-echo "multiplication is $mul"
-div=$(($a/$b))
-echo "division is $div"
+echo "choose 1-addition"
+echo "choose 2-substraction"
+echo "choose 3-division"
+echo "choose 4-division"
+read option
+case $option in
+   1)
+      sum
+      ;;
+   2)
+      sub
+      ;;
+   3)
+      mul
+      ;;
+   4)
+      div
+      ;;
+esac
